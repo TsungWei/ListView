209848b198668cb471c9560a168e65e513fca9d0
Willis <stu896117@outlook.com>
2017-08-23 15:22:08 +0800

    Create the widget's HTML template 1~5步驟
    a.更換Widget.html內 div
    b.Widget.js內 加入相關引用
    c.Widget.js內 新增function

--- Widget.js
+++ Widget.js
@@ -1,5 +1,12 @@
-define(['dojo/_base/declare', 'jimu/BaseWidget'],
-function(declare, BaseWidget) {
+define(['dojo/_base/declare',
+'jimu/BaseWidget',
+'dojo/_base/lang',
+'dojo/Deferred',
+'dgrid/OnDemandList',
+'dgrid/Selection',
+"dojo/store/Memory"],
+function(declare, BaseWidget,lang, Deferred,
+  OnDemandList, Selection, Memory) {
   //To create a widget, you need to derive from BaseWidget.
   return declare([BaseWidget], {
     // DemoWidget code goes here
@@ -11,12 +18,13 @@ function(declare, BaseWidget) {
 
     postCreate: function() {
       this.inherited(arguments);
+      //載入List
+      this.createList();
       console.log('postCreate');
     },
 
     startup: function() {
       this.inherited(arguments);
-      this.mapIdNode.innerHTML = 'map id:' + this.map.id;
       console.log('startup');
     },
 
@@ -47,6 +55,68 @@ function(declare, BaseWidget) {
 
     showVertexCount: function(count){
       this.vertexCount.innerHTML = 'The vertex count is: ' + count;
-    }
+    },
+    
+    getDataStore: function() {
+      var def = new Deferred();
+      // SAMPLE DATA
+      var SAMPLEDATA = [{
+        'id': 0,
+        'title': 'Feature 1',
+        'thumbnailImg': 'http://placehold.it/120x90'
+      }, {
+        'id': 1,
+        'title': 'Feature 2',
+        'thumbnailImg': 'http://placehold.it/120x90'
+      }];
+      def.resolve(new Memory({
+        data: SAMPLEDATA
+      }));
+      return def;
+    },
+    
+    createList: function() {
+      this.getDataStore().then(lang.hitch(this, function(datastore) {
+        var list = new (declare([OnDemandList, Selection]))({
+          'store': datastore,
+          'selectionMode': 'single',
+          'renderRow': lang.hitch(this, function (object, options) {
+            return this._createListItem(object);
+          })
+        }, this.ListNode);
+        list.startup();
+      }));
+    },
+    
+    _createListItem: function(featureObj) {
+      var listItemRoot = document.createElement('DIV');
+      listItemRoot.className = 'list-item';
+      if(featureObj) {
+        var thumbnailImgWrapper, thumbnailImg, listItemTitle;
+        // Create thumbnail
+        if(featureObj.thumbnailImg) {
+          thumbnailImgWrapper = document.createElement('div');
+          thumbnailImgWrapper.className = 'thumbnail-wrapper';
+          thumbnailImg = document.createElement('img');
+          thumbnailImg.src = featureObj.thumbnailImg;
+          thumbnailImgWrapper.appendChild(thumbnailImg);
+          listItemRoot.appendChild(thumbnailImgWrapper);
+        }
+        // Create title
+        if(featureObj.title && typeof featureObj.title === 'string') {
+          listItemTitle = document.createElement('H4');
+          listItemTitle.innerHTML = featureObj.title;
+          listItemRoot.appendChild(listItemTitle);
+          if(thumbnailImg)
+            thumbnailImg.alt = featureObj.title;
+        }
+      } else {
+        listItemRoot.innerHTML = 'NO DATA AVAILABLE';
+      }
+
+      return listItemRoot;
+    },
+    
+    
   });
 });
